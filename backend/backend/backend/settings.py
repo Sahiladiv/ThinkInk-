2b60ab203622be228a236f1bed01cf06a45e18cb
Sahil Adivarekar <63301456+Sahiladiv@users.noreply.github.com>
2025-07-14 18:59:01 -0400

    Update settings.py

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',  # for token auth
+    'corsheaders',
     'accounts',
     "onboarding",
 ]
@@ -53,6 +54,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -63,6 +65,14 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'backend.urls'
+
+CORS_ALLOWED_ORIGINS = [
+    "https://think-ink-stories.vercel.app",  
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://think-ink-stories.vercel.app",
+]
 
 TEMPLATES = [
     {
