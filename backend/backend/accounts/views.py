2793297009e1e92202bc2742a4970746aaa70a7b
Sahiladiv <sahiladivarekar17@gmail.com>
2025-07-14 15:49:33 -0400

    Authentication added in backend

---
+++ backend/backend/accounts/views.py
@@ -0,0 +1,25 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework import status
+from .serializers import RegisterSerializer
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'User registered successfully'}, status=201)
+        return Response(serializer.errors, status=400)
+
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super(LoginView, self).post(request, *args, **kwargs)
+        token = response.data['token']
+        user = Token.objects.get(key=token).user
+        return Response({
+            'token': token,
+            'username': user.username,
+            'email': user.email
+        })
